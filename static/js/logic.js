73b02c60c74922d747aff8fb95df883b27e76814
liviblocker <liviblocker@gmail.com>
2021-07-21 18:06:33 -0500

    adding toronto json file

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,12 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [37.8, -96],
+  zoom: 5
+})
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,48 +14,21 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// We create the dark view tile layer that will be an option for our map.
-let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    tileSize: 512,
-    zoomOffset: -1,    
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
 
-// Create a base layer that holds both maps.
-let baseMaps = {
-  "Streets": streets,
-  "Satellite Streets": satelliteStreets
-};
-
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-  center: [37.8, -96],
-  zoom: 5,
-  layers: [streets]
-})
-
-L.geoJson(statesData).addTo(map);
-
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
-
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/liviblocker/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+// Accessing the US States GeoJSON URL.
+let usStates = "https://raw.githubusercontent.com/liviblocker/AbortionBills/main/USBorders.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+d3.json(usStates).then(function(data) {
   console.log(data);
 
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data, {
-  fillColor: "#ffffa1",
-  fillOpacity: 0.2,
-  weight: 1,
-  onEachFeature: function(features, layer) {
-    console.log(layer);
-    layer.bindPopup("<h2>" + "Neighborhood: " + features.properties.AREA_NAME + "</h2>");
-  }
+    onEachFeature: function(features, layer) {
+        console.log(layer);
+        layer.bindPopup("<h2>" + "State: " + features.properties.NAME + "</h2>");
+      }
 }).addTo(map);
 });
