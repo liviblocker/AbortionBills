a665592b6a962bfc6575defe530b9e319c5454b2
liviblocker <liviblocker@gmail.com>
2021-07-21 19:02:07 -0500

    update us border json file

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,34 +1,34 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-  center: [37.8, -96],
-  zoom: 5
-})
-
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [37.8, -96],
+  zoom: 5,
+  layers: light
+});
 
-// Accessing the US States GeoJSON URL.
+// Accessing the Toronto neighborhoods GeoJSON URL.
 let usStates = "https://raw.githubusercontent.com/liviblocker/AbortionBills/main/USBorders.json";
 
 // Grabbing our GeoJSON data.
 d3.json(usStates).then(function(data) {
   console.log(data);
-
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data, {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    fillColor: "#ffffa1",
+    fillOpacity: 0.2,
+    weight: 1,
     onEachFeature: function(features, layer) {
-        console.log(layer);
-        layer.bindPopup("<h2>" + "State: " + features.properties.NAME + "</h2>");
-      }
-}).addTo(map);
-});
+      console.log(layer);
+      layer.bindPopup("<h2>" + "Neighborhood: " + features.properties.AREA_NAME + "</h2>");
+    }
+  }).addTo(map);
+  });
